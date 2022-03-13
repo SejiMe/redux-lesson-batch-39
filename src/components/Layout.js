198b67151a5f4dc60bee82215c9e1fa4269a7406
John Dave Decano <johndavedecano@gmail.com>
2022-03-13 15:05:20 +0800

    chore: added store

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ function Layout({ children }) {
   return (
     <div>
       <div>
-        <Link to="/">Home</Link> |<Link to="/login">Login</Link>
+        <Link to="/">Home</Link> | <Link to="/login">Login</Link>
       </div>
       {children}
       <div>Footer</div>
