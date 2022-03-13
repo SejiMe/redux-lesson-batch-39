6f26b1deddc24b12dcebbc1ab35831b192d2c425
John Dave Decano <johndavedecano@gmail.com>
2022-03-13 14:54:20 +0800

    chore: fixed layout format

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,8 +4,7 @@ function Layout({ children }) {
   return (
     <div>
       <div>
-        <Link to="/">Home</Link> |
-        <Link to="/login">Login</Link>
+        <Link to="/">Home</Link> |<Link to="/login">Login</Link>
       </div>
       {children}
       <div>Footer</div>
